e4a847498e6bd01061fe767f82cdb657fabcdb2c
nurubel1819 <rubel.18cse255@bsmrstu.edu.bd>
2024-11-13 23:07:02 +0600

    load image from server some design

--- app/src/main/java/com/example/studentserviceclintdemo/retrofit/ProductAdapter.java
+++ app/src/main/java/com/example/studentserviceclintdemo/retrofit/ProductAdapter.java
@@ -1,10 +1,12 @@
 package com.example.studentserviceclintdemo.retrofit;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.studentserviceclintdemo.R;
+import com.example.studentserviceclintdemo.activity.SingleProductViewActivity;
 import com.example.studentserviceclintdemo.model.ProductGetDto;
 
 import java.util.List;
@@ -38,10 +41,22 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
     public void onBindViewHolder(@NonNull ProductAdapter.ProductViewHolder holder, int position) {
         holder.name.setText(all_product_list.get(position).getName());
         holder.location.setText("Location :"+all_product_list.get(position).getLocation());
-        holder.price.setText(String.valueOf(all_product_list.get(position).getPrice()));
+        holder.price.setText(String.valueOf(all_product_list.get(position).getPrice())+" tk");
 
         String image_url = RetrofitInstance.getBase_url()+"file/images/"+all_product_list.get(position).getImage();
         Glide.with(context).load(image_url).into(holder.imageView);
+
+        // click singe item
+        holder.one_product_info_id.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+
+                // some change in
+                Intent intent = new Intent(context, SingleProductViewActivity.class);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -53,6 +68,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
 
         ImageView imageView;
         TextView name,location,price;
+        LinearLayout one_product_info_id;
 
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -61,6 +77,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             name = itemView.findViewById(R.id.one_product_name_id);
             location = itemView.findViewById(R.id.one_product_location_id);
             price = itemView.findViewById(R.id.one_product_price_id);
+            one_product_info_id = itemView.findViewById(R.id.one_product_info_id);
         }
     }
 }
